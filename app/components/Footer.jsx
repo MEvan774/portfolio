d4e258c82b6f6f777589c286522c640f35fb6e46
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2026-01-09 16:52:27 +0100

    Feat: added darkmode styling.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,18 +5,18 @@ import ScrollToTopButton from "./ScrollToTopButton";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#DDE3FF] text-black border-t-4 border-black">
+    <footer className="bg-[#DDE3FF] dark:bg-black text-black border-t-4 border-black dark:border-[#00AFC7]">
       <div className="py-24">
         <div className="flex flex-col gap-16 md:flex-row md:justify-between w-full max-w-2/4 mx-auto items-center">
 
           {/* Left: Contact Links */}
           <div>
-            <h2 className="mb-6 text-3xl font-semibold">Contacts</h2>
-            <div className="flex items-center gap-6 text-black">
+            <h2 className="mb-6 text-3xl font-semibold dark:text-[#00AFC7]">Contacts</h2>
+            <div className="flex items-center gap-6 text-black dark:text-[#00AFC7]">
               <a
                 href="https://nl.linkedin.com/in/milan-breuren-04223a1a5"
                 target="_blank"
-                className="hover:text-black transition"
+                className="dark:text-[#00AFC7] dark:hover:text-[#00AFC7] hover:text-black transition"
                 aria-label="LinkedIn"
               >
                 <Linkedin size={28} />
@@ -25,7 +25,7 @@ export default function Footer() {
               <a
                 href="https://github.com/MEvan774"
                 target="_blank"
-                className="hover:text-black transition"
+                className="dark:text-[#00AFC7] dark:hover:text-[#00AFC7] hover:text-black transition"
                 aria-label="GitHub"
               >
                 <Github size={28} />
@@ -33,7 +33,7 @@ export default function Footer() {
 
               <a
                 href="mailto:milanevanbreuren@gmail.com"
-                className="hover:text-black transition"
+                className="dark:text-[#00AFC7] dark:hover:text-[#00AFC7] hover:text-black transition"
                 aria-label="Email"
               >
                 <Mail size={28} />
@@ -42,14 +42,14 @@ export default function Footer() {
           </div>
 
           {/* Right: Contact Form */}
-          <div className="text-black border-4 border-black rounded-xl bg-white p-9 shadow-[4px_4px_0_0_#000] w-[340px]">
+          <div className="text-black border-4 border-black dark:border-[#00AFC7] dark:bg-black dark:text-[#00AFC7] rounded-xl bg-white p-9 shadow-[4px_4px_0_0_#000] w-[340px]">
               <h3 className="mb-6 text-2xl font-bold">
                 Let’s get in touch!
               </h3>
 
               <form action={sendEmail} className="space-y-5">
                 <div>
-                  <label className="mb-1 block text-sm text-black font-bold">
+                  <label className="mb-1 block text-sm text-black font-bold dark:text-[#00AFC7]">
                     Name
                   </label>
                   <input
@@ -57,12 +57,12 @@ export default function Footer() {
                     type="text"
                     required
                     placeholder="Enter your name..."
-                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black"
+                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black dark:focus:ring-[#00AFC7] dark:text-[#00AFC7] dark:border-[#00AFC7] dark:bg-black"
                   />
                 </div>
 
                 <div>
-                  <label className="mb-1 block text-sm text-black font-bold">
+                  <label className="mb-1 block text-sm text-black font-bold dark:text-[#00AFC7]">
                     Email
                   </label>
                   <input
@@ -70,12 +70,12 @@ export default function Footer() {
                     type="email"
                     required
                     placeholder="Enter your email..."
-                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black"
+                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black dark:focus:ring-[#00AFC7] dark:text-[#00AFC7] dark:border-[#00AFC7] dark:bg-black"
                   />
                 </div>
 
                 <div>
-                  <label className="mb-1 block text-sm text-black font-bold">
+                  <label className="mb-1 block text-sm text-black font-bold dark:text-[#00AFC7]">
                     Message
                   </label>
                   <textarea
@@ -83,7 +83,7 @@ export default function Footer() {
                     rows={4}
                     required
                     placeholder="Enter your message..."
-                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black"
+                    className="w-full border-2 border-black rounded-xl bg-white px-3 py-2 text-black outline-none focus:ring-2 focus:ring-black dark:focus:ring-[#00AFC7] dark:text-[#00AFC7] dark:border-[#00AFC7] dark:bg-black"
                   />
                 </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,33 @@ export default async function Home() {
 
   return (
     <main className="flex justify-center bg-[#E9EDFF] text-gray-900 dark:bg-black dark:text-gray-100 transition-colors">
-      <div className="w-full sm:max-w-2/4 max-w-6xl px-4 sm:px-6 lg:px-0 space-y-6 py-10">
+      <div className="sm:max-w-2/4 max-w-6xl px-4 sm:px-6 lg:px-0 space-y-6 py-10">
 
         {/* About Me Card */}
-        <section className="bg-white rounded-3xl border-4 border-black shadow-[6px_6px_0_0_#000] dark:shadow-[6px_6px_0_0_white] overflow-hidden">
-          
+        <section className="
+          bg-white
+          dark:bg-black
+          rounded-3xl
+          border-4
+          border-black
+          dark:border-[#00AFC7]
+          shadow-[6px_6px_0_0_#000]
+          dark:shadow-[0px_0px_0_0_transparent]
+          overflow-hidden
+        ">
+
           {/* Browser-like top bar */}
-          <div className="flex items-center justify-between h-12 px-4 bg-[#00AFC7] border-b-4 border-black">
+          <div className="
+            flex
+            items-center
+            justify-between
+            h-12
+            px-4
+            bg-[#00AFC7]
+            border-b-4
+            border-black
+            dark:border-[#00AFC7]
+          ">
             <h1 className="font-black text-base sm:text-lg text-black">
               About me
             </h1>
@@ -28,11 +48,11 @@ export default async function Home() {
           </div>
 
           {/* Card content */}
-          <div className="p-4 ">
+          <div className="p-4">
             <div className="flex flex-col md:flex-row gap-4 md:gap-6 justify-between">
-              
+
               {/* Text */}
-              <p className="text-sm font-semibold text-black md:w-3/5">
+              <p className="text-sm font-semibold text-black md:w-3/5 dark:text-[#00AFC7]">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -40,22 +60,54 @@ export default async function Home() {
               </p>
 
               {/* Image */}
-      <div className="flex gap-4">
-              <div className="relative h-62 aspect-[3/4] overflow-hidden rounded-3xl border-4 border-black relative">
-                <Image
-                  src="/images/profile/profile.jpg"
-                  alt="Profile photo"
-                  fill
-                  sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                  className="object-cover"
-                  priority
-                />
+              <div className="flex gap-4">
+                <div className="
+                  relative
+                  h-62
+                  aspect-[3/4]
+                  overflow-hidden
+                  rounded-3xl
+                  border-4
+                  border-black
+                  dark:border-[#00AFC7]
+                ">
+                  <Image
+                    src="/images/profile/profile.jpg"
+                    alt="Profile photo"
+                    fill
+
+
+                    sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+                    className="object-cover"
+                    priority
+                  />
+                </div>
               </div>
-      </div>
-      </div>
+
+            </div>
 
             {/* CV Button */}
-            <button className="mt-4 rounded-3xl border-2 border-black bg-[#E9EDFF] px-4 py-2 text-sm font-black text-black/80 hover:bg-[#DDE3FF] transition active:translate-x-[2px] active:translate-y-[2px]">
+            <button className="
+              mt-4
+              rounded-3xl
+              border-2
+              border-black
+              dark:border-[#00AFC7]
+              bg-[#E9EDFF]
+              dark:bg-black
+              px-4
+              py-2
+              text-sm
+              font-black
+              text-black
+              dark:text-[#00AFC7]
+              hover:bg-[#DDE3FF]
+              dark:hover:bg-[#00AFC7]
+              dark:hover:text-black
+              transition
+              active:translate-x-[2px]
+              active:translate-y-[2px]
+            ">
               CV
             </button>
           </div>
@@ -65,7 +117,7 @@ export default async function Home() {
         <SkillsSection />
 
         {/* Projects Grid */}
-        <section className="grid grid-cols-1 grid-cols-2 lg:grid-cols-3 gap-4">
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {projects.map((project) => (
             <ProjectCard key={project.slug} project={project} />
           ))}
