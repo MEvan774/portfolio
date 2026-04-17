669fdfa3dd6225c24990a1d0d597d07ef3af6d87
MEvan774 <57196554+MEvan774@users.noreply.github.com>
2026-04-17 16:35:05 +0300

    Feat: update

---
+++ portfolio-frontend/components/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav>
+        <ul>
+            <li><a href="/brain">Brain</a></li>
+            <li><a href="/nerves">Nerves</a></li>
+            <li><a href="/skin">Skin</a></li>
+        </ul>
+    </nav>
+  )
+}
